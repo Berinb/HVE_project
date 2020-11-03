0a52ed7ce56a98c7a41d9ec4ab439f29ab8efea1
muaazamjad <73789928+muaazamjad@users.noreply.github.com>
2020-11-03 19:04:06 +0100

    Analytical solution to ODE of RLC circuit with one spark gap
    Solved the ODE for one spark gap model Symbollicaly in Matlab using dsolve

--- NumericalSolution.m
+++ NumericalSolution.m
@@ -1,15 +1,8 @@
-%Here we will attempt to solve numberically the solution, using ode 
-
-%Step 1: Define parameter ranges
-%Step 2: Create Diff eq. Solver
-%Step 3: Loop throuth the parameters and obtain solution for each parameter
-%set
-%Step 4: Check the solution with the satandards
-%Step 5: If the solution passes the starndard save it if not discard
-%Step 6: Tabulate the solution that passed the standards 
+% Analytical solution of ODE 
+% Author : Muaaz Amjad
 
 
-%% An attempt to solve the differential equation 
+
 %% Symbolic I(t)
 syms I(t) 
 %% parameters
