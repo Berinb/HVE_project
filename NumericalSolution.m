e921dadb9c3936cdba11ac689a7f93daa9ca6daf
rikuda2 <73790697+rikuda2@users.noreply.github.com>
2020-11-03 18:31:38 +0100

    Update NumericalSolution.m

--- NumericalSolution.m
+++ NumericalSolution.m
@@ -1,38 +1,41 @@
 %Numerical State Space Solution
 %Author: Berin Balje 
 
-%% Pararameters
-T = 0.1;           % Simulation Time..
-x0 =  [0; 0];      % Initial Conditions
-tspan = [0, T];    % Time span 
+%% An attempt to solve the differential equation 
+%% Symbolic I(t)
+syms I(t) 
+%% parameters
 
-R = 100;               
-L = 100e-3;           
-C = 50e-6;            
-Vi = 100;             % Input Voltage
+L = 1e-6;
+C = 1e-6;
+Cp= 2e-3;
+R = 50;
+Rp =50;
+V1 = 10000;
+%% Representing Derivative by creating symbolic function 
+Di = diff(I);
+%% ODE
+ode = L*diff(I,t,2)+ (R+Rp)*diff(I,t)+(1/C)*I(t) == 0;
+%% ICs
+cond1 = I(0)==0;
+cond2 = L*Di(0)==V1;
+conds = [cond1 cond2];
+%% Solve ODE for I
+iSol(t) = dsolve(ode,conds);
+iSol = simplify(iSol);
+%% Plotting 
+t = linspace(0,350e-6);  % for a 10/350 pulse. At t = 350us the current amplitude should be 50% of the I_peak
+figure(1)
+plot(t,iSol(t));
 
-%% RLC Circuit Dynamic - State-Space Model
+%% Q and W/R within 5 ms 
 
-% y(1) = d (I_ind);    y(2) = d (V_cap);
-%        /dt               /dt
-y =@(t,x) [-R/L, -1/L; 1/C, 0]*x + [1/L; 0]*Vi;
+Q = int(iSol,0,5e-3) ;% copy the answer in the command window and click enter to get the numerical result 
 
-%% ODE Solver 
-[t, x] = ode45(@(t,x) y(t,x), tspan, x0); 
-
-% Plot Current thru Inductor & Voltage across Capacitor
-I_ind = x(:,1);    V_cap = x(:,2);
-
-subplot(2,1,1);
-plot(t, V_cap, 'b', 'linewidth',0.75); 
-grid on;
-title('Series RLC Circuit - Voltage across Capacitor'); 
-xlabel('Time (sec)');
-ylabel('Voltage (V)');
-    
-subplot(2,1,2);
-plot(t, I_ind, 'r', 'linewidth',0.75);
-grid on;
-title('Series RLC Circuit - Current thru Inductor'); 
-xlabel('Time (sec)');
-ylabel('Current (A)');
+%% ode45 Solution (Parasitic effect of components)
+syms y(t)
+[V] = odeToVectorField(diff(y, 2) == (R+Rp)*diff(y,t)+(1/(C+Cp)*y(t)));
+M = matlabFunction(V,'vars', {'t','Y'});
+sol = ode45(M,[0 355e-6],[10 0]);
+figure(2)
+fplot(@(x)deval(sol,x,1), [0, 355e-6])
