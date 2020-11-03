8b4a8ab9d0a36289629708705e4009c8881e775c
muaazamjad <73789928+muaazamjad@users.noreply.github.com>
2020-11-03 22:18:52 +0100

    Exponential decay part
    most updated version.

--- NumericalSolution.m
+++ NumericalSolution.m
@@ -7,7 +7,7 @@
 syms I(t) 
 %% parameters
 R = 100;               
-L = 1e-3;           
+L =1e-3;           
 C = 50e-6;            
 Vi = 100;
 tau = L/R;
@@ -27,32 +27,32 @@ iSol(t) = dsolve(ode,conds);
 v = (1/C)*int(iSol)+Vi;
 
 %% rise time array 
-t = linspace(0,0.00005);  % from t = 0 to t = t1 ( rise time of pulse)
+t = linspace(0,5e-5);  % from t = 0 to t = t1 ( rise time of pulse)
 
-%% Exponentially decaying part 
+%% Exponentially decaying part RL circuit when SG2 is ignited
 i_peak = double(max(iSol(t)));
-t_fall = linspace(0,350e-6);
+t_fall = linspace(0,3e-4);
 i_exp = i_peak*(exp(-t_fall/tau));
 
 %% Plotting 
 subplot(2,1,2)
 hold on
 plot(t,iSol(t));
-plot(linspace(t(end),350e-6),i_exp);
+plot(linspace(t(end),3.5e-4),i_exp);
 hold off
 title('Inductor current')
 xlabel('Time[s]')
 ylabel('i(t)')
 
-subplot(2,1,1)
- 
-plot(t,v(t))
-title('Voltage across capacitor')
-xlabel('Time[s]')
-ylabel('v(t)')
+% subplot(2,1,1)
+%  
+% plot(t,v(t))
+% title('Voltage across capacitor')
+% xlabel('Time[s]')
+% ylabel('v(t)')
 
 %% Q and W/R within 5 ms 
 
-Q = int(iSol,0,5e-3) % copy the answer in the command window and click enter to get the numerical result 
+% Q = int(iSol,0,5e-3) % copy the answer in the command window and click enter to get the numerical result 
 % Q = 0.1000 . From Zeller Q = I_peak*5e-4 = 200*5e-4 = 0.1000
 
